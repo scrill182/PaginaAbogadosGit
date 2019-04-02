f9e3fc32ba4c9fa998953926bea97f39e8ec37f5
rill182 <scrill182@gmail.com>
2019-04-02 17:50:38 -0600

    Se cambiaron referencias al antiguo nombre al definitivo ZAOS Abogados

--- contact_process.php
+++ contact_process.php
@@ -15,13 +15,13 @@
 
     $subject = "You have a message from your Bitmap Photography.";
 
-    $logo = 'img/logo.png';
+    $ZAOSAbogados_logo = 'img/ZAOSAbogados_logo.png';
     $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "<a href='{$link}'><img src='{$ZAOSAbogados_logo}' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
